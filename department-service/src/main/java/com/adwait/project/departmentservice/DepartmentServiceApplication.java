54ed2d50ec72df324e07851a6b0b9e01924ce821
Adwait <adwait.kul.2018@gmail.com>
2023-04-17 16:45:55 -0700

    register department service with service registry

--- department-service/src/main/java/com/adwait/project/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/adwait/project/departmentservice/DepartmentServiceApplication.java
@@ -3,9 +3,11 @@ package com.adwait.project.departmentservice;
 import org.modelmapper.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.*;
 import org.springframework.context.annotation.*;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DepartmentServiceApplication {
 
     @Bean
