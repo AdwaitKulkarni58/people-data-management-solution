1abb14e10d12d10ab80e6182bdacf63be7ca9ccb
Adwait <adwait.kul.2018@gmail.com>
2023-04-18 15:00:20 -0700

    add config server as a eureka client

--- config-server/src/main/java/com/adwait/project/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/adwait/project/configserver/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.adwait.project.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.*;
+import org.springframework.cloud.netflix.eureka.*;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
