09545ddbb844e1fa187a8a2c5ac07f39c21883cf
Adwait <adwait.kul.2018@gmail.com>
2023-04-19 18:10:27 -0700

    registered organization service with Eureka, added API Gateway routes, created external configs on config server

--- organization-service/src/main/java/com/adwait/project/organizationservice/OrganizationServiceApplication.java
+++ organization-service/src/main/java/com/adwait/project/organizationservice/OrganizationServiceApplication.java
@@ -3,9 +3,11 @@ package com.adwait.project.organizationservice;
 import org.modelmapper.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.*;
 import org.springframework.context.annotation.*;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrganizationServiceApplication {
 
     @Bean
