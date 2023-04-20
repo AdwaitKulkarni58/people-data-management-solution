e6ae96c016f028e3ce769d82000439a0b6dee222
Adwait <adwait.kul.2018@gmail.com>
2023-04-19 17:56:00 -0700

    expose REST Api

--- organization-service/src/main/java/com/adwait/project/organizationservice/rest/OrganizationController.java
+++ organization-service/src/main/java/com/adwait/project/organizationservice/rest/OrganizationController.java
@@ -1,4 +1,27 @@
 package com.adwait.project.organizationservice.rest;
 
+import com.adwait.project.organizationservice.dto.*;
+import com.adwait.project.organizationservice.service.*;
+import lombok.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/organizations")
 public class OrganizationController {
+
+    private OrganizationService organizationService;
+
+    @PostMapping
+    public ResponseEntity<OrganizationDto> saveOrganization(@RequestBody OrganizationDto organizationDto) {
+        OrganizationDto savedOrganization = organizationService.saveOrganization(organizationDto);
+        return new ResponseEntity<>(savedOrganization, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{organization-code}")
+    public ResponseEntity<OrganizationDto> getOrganization(@PathVariable(name = "organization-code") String organizationCode) {
+        OrganizationDto organizationDto = organizationService.getOrganizationByCode(organizationCode);
+        return new ResponseEntity<>(organizationDto, HttpStatus.OK);
+    }
 }
